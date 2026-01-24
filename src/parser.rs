291ebb94b4391649d23eb9e6ee91b54effd5a0c1
Lokesh0224 <lokeshk20019@gmail.com>
2026-01-25 00:09:01 +0530

    '\'' parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,8 +1,10 @@
+
 pub fn parse_input(input: &str) -> Vec<String> {
     let mut args =  Vec::new();
     let mut current = String::new();
     let mut in_single_quote = false;
 
+    //we're reading each char through chars()(interation)
     let mut chars = input.chars().peekable();
 
     while let Some(c) = chars.next(){
@@ -12,9 +14,26 @@ pub fn parse_input(input: &str) -> Vec<String> {
             }
 
             ' ' | '\t' if !in_single_quote => {
-                
+                if !current.is_empty(){
+                    args.push(current.clone());
+                    current.clear();
+                }
+
+                //this checks after a space or tab there is another space or tab through peek(), if it is it passes through next() 
+                while let Some(' ' | '\t') = chars.peek() {
+                    chars.next();
+                }
+
+            }, 
+
+            _ => {
+                current.push(c);
             }
         }
+
+    }
+    if !current.is_empty() {
+        args.push(current);
     }
 
     args
