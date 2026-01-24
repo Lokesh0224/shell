cb3f93bb14d37388f729d3dfa2bf72cb3e587475
Lokesh0224 <lokeshk20019@gmail.com>
2026-01-25 00:29:03 +0530

    now for double quotes

--- src/parser.rs
+++ src/parser.rs
@@ -2,18 +2,25 @@
 pub fn parse_input(input: &str) -> Vec<String> {
     let mut args =  Vec::new();
     let mut current = String::new();
+
     let mut in_single_quote = false;
+    let mut in_double_quote = false;
 
     //we're reading each char through chars()(interation)
     let mut chars = input.chars().peekable();
 
     while let Some(c) = chars.next(){
         match c {
-            '\'' =>{
+
+            '\'' if !in_double_quote =>{
                 in_single_quote = !in_single_quote;
+            }, 
+
+            '"' if !in_single_quote => {
+                in_double_quote = !in_double_quote;
             }
 
-            ' ' | '\t' if !in_single_quote => {
+            ' ' | '\t' if !in_single_quote && !in_double_quote => {
                 if !current.is_empty(){
                     args.push(current.clone());
                     current.clear();
