68697fa3b5a6621cf81375bc1a5c54dfe9ab8fcd
Lokesh0224 <lokeshk20019@gmail.com>
2026-01-27 15:46:02 +0530

    stdout_append

--- src/stdStreams.rs
+++ src/stdStreams.rs
@@ -8,6 +8,8 @@ pub struct Redirection{
     pub clean_args: Vec<String>, 
     pub stdout_file: Option<String>,
     pub stderr_file: Option<String>,
+    pub stdout_append: bool, 
+    //pub stdapnderr_file: Option<String>,
 }
 
 impl Redirection{
@@ -15,6 +17,8 @@ impl Redirection{
         let mut clean_args = Vec::new();
         let mut stdout_file = None;
         let mut stderr_file = None;
+        let mut stdout_append = false;
+        //let mut stdapnderr_file = None;
 
         let mut i =0;
         while i <args.len() {
@@ -22,6 +26,7 @@ impl Redirection{
                 ">" | "1>" =>{
                     if i+1 < args.len(){
                         stdout_file = Some(args[i+1].clone());
+                        stdout_append = false;
                         i+=2;
                     }else{
                         eprintln!("Syntax error: no file after >");
@@ -38,7 +43,23 @@ impl Redirection{
                         eprintln!("Syntax error: no file after 2>");
                         break;
                     }
-                }
+                }, 
+
+                ">>" | "1>>" => {
+                    if i+1 <args.len(){
+                        stdout_file = Some(args[i+1].clone());
+                        stdout_append = true;
+
+                        i += 2;
+                    }
+                },
+
+                // "2>>" => {
+                //     if i+1 <args.len(){
+                //         stdapnderr_file = Some(args[i+1].clone());
+                //         i += 2;
+                //     }
+                // }
 
                 _ => {
                     clean_args.push(args[i].clone());
@@ -49,16 +70,22 @@ impl Redirection{
             
         }
 
-        Self {clean_args, stdout_file, stderr_file}
+        Self {clean_args, stdout_file, stderr_file, stdout_append}
     }
 
     pub fn prepare_redirections(&self){
         if let Some(file) = &self.stdout_file{
-            let _ = OpenOptions::new()
-                    .create(true)
-                    .write(true)
-                    .truncate(true)
-                    .open(file);
+            let mut opts = OpenOptions::new();
+            opts.create(true).write(true);
+
+            if self.stdout_append{
+                opts.append(true);
+            }
+            else{
+                opts.truncate(true);
+            }
+
+            let _ = opts.open(file);
         }
 
         if let Some(file) = &self.stderr_file{
@@ -73,14 +100,11 @@ impl Redirection{
     //echo hello > output.txt
     pub fn write_builtin_output(&self, text: &str){
         if let Some(file) = &self.stdout_file{
-            if let Ok(mut f) = OpenOptions::new()
-                .create(true)
-                .write(true)
-                .truncate(true)
-                .open(file)
-                {
-                    writeln!(f, "{}", text).ok();
-                }
+            let mut opts = OpenOptions::new();
+            opts.append(true); // always append now (file already truncated if needed)
+            if let Ok(mut f) = opts.open(file){
+                writeln!(f, "{}", text).ok();
+            }
         }
         else{
             println!("{}", text);
@@ -110,15 +134,18 @@ impl Redirection{
     pub fn apply_to_no_builtin(&self, cmd: &mut Command){
         //cmd represents a child process being prepared
         if let Some(file) = &self.stdout_file{
-            if let Ok(f) = OpenOptions::new()
-                .create(true)
-                .write(true)
-                .truncate(true)
-                .open(file)
-                {
-                    //but here we don't know what is the output string, so run the child process and then 
-                    cmd.stdout(Stdio::from(f));               //When this command runs, instead of printing to the terminal, write its output into this file.
-                }
+            let mut opts = OpenOptions::new();
+            opts.write(true).create(true);
+
+            if self.stdout_append {
+                opts.append(true);
+            } else {
+                opts.truncate(true);
+            }
+
+            if let Ok(f) = opts.open(file) {
+                cmd.stdout(Stdio::from(f));
+            }
         }
 
         if let Some(file) = &self.stderr_file{
